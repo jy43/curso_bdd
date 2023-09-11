62bdc6bd0d5fe7ed10f3f390586fc1383ea3dca9
Jayson <jayson.edf@gmail.com>
2023-09-11 17:29:03 -0300

    relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,11 @@ const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     baseUrl: 'https://automationpratice.com.br/',
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       addCucumberPreprocessorPlugin(on, config);
       on(
         "file:preprocessor",
